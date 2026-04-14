d7239a8fd0906e0cfda1fb83698db43fa2e7c006
Rafael Bula <rafabula1@gmail.com>
2026-04-14 14:22:00 -0400

    feat: documentos .h añadidos, miembro_familia.cpp añadido

---
+++ src/arbol_familia.h
@@ -0,0 +1,69 @@
+#ifndef ARBOL_FAMILIA_H
+#define ARBOL_FAMILIA_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "miembro_mafia.h"
+
+template<class T>
+class Node {
+private:
+    T data;
+    int height;
+    int fe;
+    Node<T> *left;
+    Node<T> *right;
+
+    int calculateHeight(Node<T>* node);
+
+public:
+    enum Ramas { IZQ = 0, DER = 1 };
+
+    Node(T data, Node<T>* right, Node<T>* left);
+    Node<T>* getChild(int child);
+    void setLeft(Node<T>* left);
+    void setRight(Node<T>* right);
+    void setData(T data);
+    T getData() const;
+    void setFe();
+    int getFe() const;
+    int getHeight() const;
+    void setHeight(int h);
+    bool isLeaf() const;
+};
+
+template<class T>
+class ArbolFamilia {
+private:
+    Node<T>* root;
+    enum { IZQ = 0, DER = 1 };
+
+    
+    Node<T>* rotationSimpleIzq(Node<T>* node, Node<T>* child);
+    Node<T>* rotationSimpleDer(Node<T>* node, Node<T>* child);
+    Node<T>* rotationDoubleIzq(Node<T>* node, Node<T>* child);
+    Node<T>* rotationDoubleDer(Node<T>* node, Node<T>* child);
+    
+   
+    Node<T>* insert(Node<T>* node, T data, bool& heightChanged);
+    Node<T>* buscarNodo(Node<T>* node, int id);
+    void inOrden(Node<T>* node);
+    Node<T>* buscarSucesor(Node<T>* node, bool buscarEnPrison);
+
+public:
+    ArbolFamilia();
+    void insertar(T data);
+    Node<T>* buscar(int id);
+    void mostrarLineaSucesion();
+    Node<T>* encontrarPrimerSucesor();
+    Node<T>* encontrarSucesorEnPrison();
+    void actualizarJefatura(Node<T>* nuevoJefe);
+    Node<T>* getRoot();
+};
+
+
+#include "arbol_familia.cpp"
+
+#endif
